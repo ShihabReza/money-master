d6fbcb65b0263faff17178d4188c6a7a5304f336
ShihabReza <shihabreza457@gmail.com>
2022-02-16 23:06:33 +0600

    totalAmount

--- income.js
+++ income.js
@@ -1,3 +1,22 @@
 document.getElementById('calculete-btn').addEventListener('click', function(){
-    console.log('hello')
+   
+    const foodTk = document.getElementById('food')
+    const foodValue = foodTk.value;
+    const foodinc = parseFloat(foodValue);
+   
+    const rentTk = document.getElementById('rent');
+    const rentValue = rentTk.value;
+    const restFlot = parseFloat(rentValue);
+
+    const ClothesTk = document.getElementById('Clothes');
+    const ClothesValue = ClothesTk.value;
+    const ClothesIns = parseFloat(ClothesValue);
+
+    const totalTk = foodinc+restFlot+ClothesIns
+    
+    const totalAmount = document.getElementById('total-amound');
+    const totalValue = totalAmount.innerText;
+
+    totalAmount.innerText = totalTk
+    
 })
